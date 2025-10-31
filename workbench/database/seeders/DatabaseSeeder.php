6f727f5f04a429aacaabef3e006d19169a91b276
Martin Weinschenk <martin.weinschenk@farbcode.net>
2025-10-31 15:32:24 +0100

    chore: code cleanup, improved github workflows

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Workbench\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Workbench\Workbench\Workbench\Database\Factories\UserFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // UserFactory::new()->times(10)->create();
 
-        UserFactory::new()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
